1d50ecdfb583f006f4a2df37fd7b14119e670ee1
Rafael <rafael.markjames.bscs2022@gmail.com>
2024-12-17 02:04:02 +0800

    Arjaypogi

--- src/components/L.jsx
+++ src/components/L.jsx
@@ -19,7 +19,7 @@ const LoginForm = () => {
   };
   const handleSubmit = async (e) => {
   e.preventDefault();
-
+    
   try {
     const response = await fetch("http://localhost:5000/api/login", {
       method: "POST",
